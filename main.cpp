5eca53b6be0e3150f994fa9ead2227788e5cf6f0
Forman37 <forman3@Me.com>
2026-03-14 11:00:06 -0400

    Commit before branch

--- main.cpp
+++ main.cpp
@@ -40,53 +40,57 @@ int main() {
 	int choice;
 	int idCounter = 1;
 
-	while (true) {
-		printTasks(tasks);
-		std::cout << "\n1. Add Task\n2. Update Task\n3. Remove Task\n4. Complete\n5. Transfer Completed Tasks to Save File\n6. Exit\n";
-		std::cout << "Enter Choice: ";
+	try {
+		while (true) {
+			printTasks(tasks);
+			std::cout << "\n1. Add Task\n2. Update Task\n3. Remove Task\n4. Complete\n5. Transfer Completed Tasks to Save File\n6. Exit\n";
+			std::cout << "Enter Choice: ";
 
-		if (!(std::cin >> choice)) {
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			std::cout << "Invalid input!\n";
-			continue;
-		}
+			if (!(std::cin >> choice)) {
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				std::cout << "Invalid input!\n";
+				continue;
+			}
 
-		switch (static_cast<MenuOptions>(choice)) {
-			case MenuOptions::ADD: {
-				idCounter = TaskManager::addTask(tasks, idCounter);
-				break;
-			}
-			case MenuOptions::UPDATE: {
-				TaskManager::updateTask(tasks);
-				break;
-			}
-			case MenuOptions::REMOVE: {
-				TaskManager::deleteTask(tasks);
-				break;
-			}
-			case MenuOptions::COMPLETE: {
-				TaskManager::completeTask(tasks);
-				break;
-			}
-			case MenuOptions::MOVECOMPLETED: {
-				std::vector<Task> completedTasks = TaskManager::moveCompleted(tasks);
-				if (!completedTasks.empty()) {
-					Storage::saveTasks(completedTasks, "completedTasks.txt", false);
+			switch (static_cast<MenuOptions>(choice)) {
+				case MenuOptions::ADD: {
+					idCounter = TaskManager::addTask(tasks, idCounter);
+					break;
 				}
-				break;
-			}
-			case MenuOptions::EXIT: {
-				Storage::saveTasks(tasks, "tasks.txt", true);
-				// Exit
-				return 0;
-			}
-			default: {
-				std::cout << "\nInvalid choice!" << std::endl;
-				break;
+				case MenuOptions::UPDATE: {
+					TaskManager::updateTask(tasks);
+					break;
+				}
+				case MenuOptions::REMOVE: {
+					TaskManager::deleteTask(tasks);
+					break;
+				}
+				case MenuOptions::COMPLETE: {
+					TaskManager::completeTask(tasks);
+					break;
+				}
+				case MenuOptions::MOVECOMPLETED: {
+					std::vector<Task> completedTasks = TaskManager::moveCompleted(tasks);
+					if (!completedTasks.empty()) {
+						Storage::saveTasks(completedTasks, "completedTasks.txt", false);
+					}
+					break;
+				}
+				case MenuOptions::EXIT: {
+					Storage::saveTasks(tasks, "tasks.txt", true);
+					// Exit
+					return 0;
+				}
+				default: {
+					std::cout << "\nInvalid choice!" << std::endl;
+					break;
+				}
 			}
 		}
-	}
+	}catch (...) {
 
-	return 0;
+	}finally {
+
+	}
 }
