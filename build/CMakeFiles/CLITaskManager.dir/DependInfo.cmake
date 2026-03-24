6c23b68d4a19b2018ca042fcc4b8875b8ae0084c
Forman37 <forman3@Me.com>
2026-03-24 06:33:09 -0400

    Added SQL database changing and smoothed out SQL Database

---
+++ build/CMakeFiles/CLITaskManager.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jamesforman/dev/CLionProjects/CLITaskManager/third_party/sqlite/sqlite3.c" "CMakeFiles/CLITaskManager.dir/third_party/sqlite/sqlite3.c.o" "gcc" "CMakeFiles/CLITaskManager.dir/third_party/sqlite/sqlite3.c.o.d"
+  "/Users/jamesforman/dev/CLionProjects/CLITaskManager/Storage.cpp" "CMakeFiles/CLITaskManager.dir/Storage.cpp.o" "gcc" "CMakeFiles/CLITaskManager.dir/Storage.cpp.o.d"
+  "/Users/jamesforman/dev/CLionProjects/CLITaskManager/Task.cpp" "CMakeFiles/CLITaskManager.dir/Task.cpp.o" "gcc" "CMakeFiles/CLITaskManager.dir/Task.cpp.o.d"
+  "/Users/jamesforman/dev/CLionProjects/CLITaskManager/TaskManager.cpp" "CMakeFiles/CLITaskManager.dir/TaskManager.cpp.o" "gcc" "CMakeFiles/CLITaskManager.dir/TaskManager.cpp.o.d"
+  "/Users/jamesforman/dev/CLionProjects/CLITaskManager/main.cpp" "CMakeFiles/CLITaskManager.dir/main.cpp.o" "gcc" "CMakeFiles/CLITaskManager.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CLITaskManager.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CLITaskManager"
+  "CLITaskManager.pdb"
+  "CMakeFiles/CLITaskManager.dir/Storage.cpp.o"
+  "CMakeFiles/CLITaskManager.dir/Storage.cpp.o.d"
+  "CMakeFiles/CLITaskManager.dir/Task.cpp.o"
+  "CMakeFiles/CLITaskManager.dir/Task.cpp.o.d"
+  "CMakeFiles/CLITaskManager.dir/TaskManager.cpp.o"
+  "CMakeFiles/CLITaskManager.dir/TaskManager.cpp.o.d"
+  "CMakeFiles/CLITaskManager.dir/main.cpp.o"
+  "CMakeFiles/CLITaskManager.dir/main.cpp.o.d"
+  "CMakeFiles/CLITaskManager.dir/third_party/sqlite/sqlite3.c.o"
+  "CMakeFiles/CLITaskManager.dir/third_party/sqlite/sqlite3.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/CLITaskManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
